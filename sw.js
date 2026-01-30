b392cd268d0f849a0616727f96f22dc6162234b1
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-01-30 14:45:30 -0600

    172

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
 importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  self.clients.claim();
+});
 async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
+  await scramjet.loadConfig();
+  if (scramjet.route(event)) {
+    return scramjet.fetch(event);
+  }
+  if (event.request.mode === "navigate") {
+    return fetch(event.request);
+  }
+  return fetch(event.request);
 }
 self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+  event.respondWith(handleRequest(event));
 });
