5c0c9d3dcb185e7f42c58dbda4986b7f0818997a
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-02-16 17:20:25 -0600

    280 (test)

--- games.js
+++ games.js
@@ -297,6 +297,16 @@ document.addEventListener("DOMContentLoaded", function () {
         showGames("1");
     });
 })
+const loader = document.createElement("div");
+loader.id = "planet-loader";
+loader.innerHTML = `
+    <div class="planet-wrapper">
+        <div class="ring ring1"></div>
+        <div class="ring ring2"></div>
+        <div class="ring ring3"></div>
+        <div class="letter">C</div>
+    </div>
+`;
 function showLoader() {
     if (!document.getElementById("planet-loader")) {
         document.body.prepend(loader);
