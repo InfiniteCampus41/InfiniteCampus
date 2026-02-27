c186a3cca23a1a8412285e1c7dffd0aea7b052d7
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-02-27 13:23:04 -0600

    408 (test)

--- sw.js
+++ sw.js
@@ -2,37 +2,41 @@ importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 self.addEventListener("install", (event) => {
-  	self.skipWaiting();
+    self.skipWaiting();
 });
 self.addEventListener("activate", (event) => {
-  	self.clients.claim();
+    self.clients.claim();
 });
-// async function handleRequest(event) {
-//     const url = new URL(event.request.url);
-//     if (
-//     	url.pathname.startsWith("/games/slope/Build/") ||
-//     	url.pathname.startsWith("/games/slope/TemplateData/") ||
-//     	url.pathname.includes("unityloader") ||
-//     	url.pathname.endsWith(".wasm")
-// 	) {
-//     	return fetch(event.request);
-// 	}
-//     await scramjet.loadConfig();
-//     if (scramjet.route(event)) {
-//         return scramjet.fetch(event);
-//     }
-//     return fetch(event.request);
-// }
 async function handleRequest(event) {
-  	await scramjet.loadConfig();
-  	if (scramjet.route(event)) {
-	  	return scramjet.fetch(event);
-  	}
-  	if (event.request.mode === "navigate") {
-    	return fetch(event.request);
-  	}
-  	return fetch(event.request);
+    const req = event.request;
+    const url = new URL(req.url);
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+    const unityExtensions = [
+        "slope.json",
+		"unityloader41.js",
+		"UnityProgress.js",
+        ".unityweb",
+        ".framework.js",
+        ".loader.js",
+        ".symbols.json"
+    ];
+    const isUnityAsset = unityExtensions.some(ext =>
+        url.pathname.endsWith(ext)
+    );
+    if (req.headers.has("range")) {
+        return fetch(req);
+    }
+    if (isUnityAsset) {
+        return fetch(req);
+    }
+    if (req.mode === "navigate") {
+        return fetch(req);
+    }
+    return fetch(req);
 }
 self.addEventListener("fetch", (event) => {
-  	event.respondWith(handleRequest(event));
+    event.respondWith(handleRequest(event));
 });
