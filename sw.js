8769e1bf177ee2dc2061c51d11da0c1388b315d8
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-02-27 13:01:20 -0600

    403 (test)

--- sw.js
+++ sw.js
@@ -8,15 +8,32 @@ self.addEventListener("activate", (event) => {
   	self.clients.claim();
 });
 async function handleRequest(event) {
-  	await scramjet.loadConfig();
-  	if (scramjet.route(event)) {
-	  	return scramjet.fetch(event);
-  	}
-  	if (event.request.mode === "navigate") {
-    	return fetch(event.request);
-  	}
-  	return fetch(event.request);
+    const url = new URL(event.request.url);
+    if (
+        url.pathname.startsWith("/games/slope/Build/") ||
+        url.pathname.startsWith("/games/slope/TemplateData/") ||
+        url.pathname.endsWith(".wasm") ||
+        url.pathname.endsWith(".data") ||
+        url.pathname.includes("unityloader")
+    ) {
+        return fetch(event.request);
+    }
+    await scramjet.loadConfig();
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+    return fetch(event.request);
 }
+// async function handleRequest(event) {
+//   	await scramjet.loadConfig();
+//   	if (scramjet.route(event)) {
+// 	  	return scramjet.fetch(event);
+//   	}
+//   	if (event.request.mode === "navigate") {
+//     	return fetch(event.request);
+//   	}
+//   	return fetch(event.request);
+// }
 self.addEventListener("fetch", (event) => {
   	event.respondWith(handleRequest(event));
 });
