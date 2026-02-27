a57fdc89423139a503551bf9a467f09a20809116
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-02-27 13:40:09 -0600

    409

--- sw.js
+++ sw.js
@@ -2,41 +2,21 @@ importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 self.addEventListener("install", (event) => {
-    self.skipWaiting();
+  	self.skipWaiting();
 });
 self.addEventListener("activate", (event) => {
-    self.clients.claim();
+  	self.clients.claim();
 });
 async function handleRequest(event) {
-    const req = event.request;
-    const url = new URL(req.url);
-    await scramjet.loadConfig();
-    if (scramjet.route(event)) {
-        return scramjet.fetch(event);
-    }
-    const unityExtensions = [
-        "slope.json",
-		"unityloader41.js",
-		"UnityProgress.js",
-        ".unityweb",
-        ".framework.js",
-        ".loader.js",
-        ".symbols.json"
-    ];
-    const isUnityAsset = unityExtensions.some(ext =>
-        url.pathname.endsWith(ext)
-    );
-    if (req.headers.has("range")) {
-        return fetch(req);
-    }
-    if (isUnityAsset) {
-        return fetch(req);
-    }
-    if (req.mode === "navigate") {
-        return fetch(req);
-    }
-    return fetch(req);
+  	await scramjet.loadConfig();
+  	if (scramjet.route(event)) {
+	  	return scramjet.fetch(event);
+  	}
+  	if (event.request.mode === "navigate") {
+    	return fetch(event.request);
+  	}
+  	return fetch(event.request);
 }
 self.addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event));
+  	event.respondWith(handleRequest(event));
 });
