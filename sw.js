521ad070089e32c1b36b0389c57c98eb7fcff841
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-02-27 13:11:48 -0600

    407 (revert)

--- sw.js
+++ sw.js
@@ -7,32 +7,32 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   	self.clients.claim();
 });
-async function handleRequest(event) {
-    const url = new URL(event.request.url);
-    if (
-    	url.pathname.startsWith("/games/slope/Build/") ||
-    	url.pathname.startsWith("/games/slope/TemplateData/") ||
-    	url.pathname.includes("unityloader") ||
-    	url.pathname.endsWith(".wasm")
-	) {
-    	return fetch(event.request);
-	}
-    await scramjet.loadConfig();
-    if (scramjet.route(event)) {
-        return scramjet.fetch(event);
-    }
-    return fetch(event.request);
-}
 // async function handleRequest(event) {
-//   	await scramjet.loadConfig();
-//   	if (scramjet.route(event)) {
-// 	  	return scramjet.fetch(event);
-//   	}
-//   	if (event.request.mode === "navigate") {
+//     const url = new URL(event.request.url);
+//     if (
+//     	url.pathname.startsWith("/games/slope/Build/") ||
+//     	url.pathname.startsWith("/games/slope/TemplateData/") ||
+//     	url.pathname.includes("unityloader") ||
+//     	url.pathname.endsWith(".wasm")
+// 	) {
 //     	return fetch(event.request);
-//   	}
-//   	return fetch(event.request);
+// 	}
+//     await scramjet.loadConfig();
+//     if (scramjet.route(event)) {
+//         return scramjet.fetch(event);
+//     }
+//     return fetch(event.request);
 // }
+async function handleRequest(event) {
+  	await scramjet.loadConfig();
+  	if (scramjet.route(event)) {
+	  	return scramjet.fetch(event);
+  	}
+  	if (event.request.mode === "navigate") {
+    	return fetch(event.request);
+  	}
+  	return fetch(event.request);
+}
 self.addEventListener("fetch", (event) => {
   	event.respondWith(handleRequest(event));
 });
