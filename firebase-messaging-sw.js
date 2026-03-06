bcff63cf5133a7efc3d0f7fd6e452ffe1cd897ff
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2026-03-05 22:24:11 -0600

    454 (test)

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
+firebase.initializeApp({
+  	apiKey: "AIzaSyBvbTQcsL1DoipWlO0ckApzkwCZgxBYbzY",
+  	authDomain: "notes-27f22.firebaseapp.com",
+  	projectId: "notes-27f22",
+  	messagingSenderId: "424229778181",
+  	appId: "1:424229778181:web:fa531219ed165346fa7d6c"
+});
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  	self.registration.showNotification(payload.notification.title, {
+    	body: payload.notification.body,
+    	icon: "/icon.png"
+  	});
+});
--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
+firebase.initializeApp({
+  	apiKey: "AIzaSyBvbTQcsL1DoipWlO0ckApzkwCZgxBYbzY",
+  	authDomain: "notes-27f22.firebaseapp.com",
+  	projectId: "notes-27f22",
+  	messagingSenderId: "424229778181",
+  	appId: "1:424229778181:web:fa531219ed165346fa7d6c"
+});
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  	self.registration.showNotification(payload.notification.title, {
+    	body: payload.notification.body,
+    	icon: "/res/192icon.png"
+  	});
+});
 importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
